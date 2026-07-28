f942e2cb4cdc56fda650add6e6ceefa288abecf9
Kevin Sun <55240339+shmilyhua@users.noreply.github.com>
2026-07-29 02:37:23 +0800

    Update URL for home page request in init_client_transaction

--- graphql_api.py
+++ graphql_api.py
@@ -48,7 +48,7 @@ class GraphqlAPI():
     def init_client_transaction(cls):
         session = requests.Session()
         session.headers = generate_headers()
-        home_page = session.get(url="https://x.com")
+        home_page = session.get(url="https://x.com/home")
         home_page_response = bs4.BeautifulSoup(home_page.content, 'html.parser')
         ondemand_file_url = get_ondemand_file_url(response=home_page_response)
         ondemand_file = session.get(url=ondemand_file_url)
