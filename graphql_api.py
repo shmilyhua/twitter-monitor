76de9d84adc8948e5c1014894b88defb64464471
Kevin Sun <55240339+shmilyhua@users.noreply.github.com>
2026-09-26 18:55:09 +0800

    Add get_clint_transaction_id method to ClientTransaction

--- graphql_api.py
+++ graphql_api.py
@@ -102,6 +102,7 @@ class GraphqlAPI():
             ondemand_file_response = ondemand_file.text
             cls.ct = ClientTransaction(home_page_response=home_page_response,
                                        ondemand_file_response=ondemand_file_response)
+
     @classmethod
     def get_clint_transaction_id(cls, method: str, url: str) -> str:
         return cls.ct.generate_transaction_id(method=method,
